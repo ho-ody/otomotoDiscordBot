572188b6b282d5b14956b8a345aa9fc4e899bba2
ho-ody <r4f4l135@gmail.com>
2023-04-19 15:49:42 +0200

    added functional adding and canceling searches with embed responses from bot

--- SearchTarget.py
+++ SearchTarget.py
@@ -1,8 +1,9 @@
 class SearchTarget:
-    def __init__(self, search_url, channel_id, user_id):
+    def __init__(self, search_url, channel_id, user_id, add_date):
         self.search_url = search_url
         self.channel_id = channel_id
         self.user_id = user_id
+        self.add_date = add_date
 
 
 
--- bot.py
+++ bot.py
@@ -8,12 +8,15 @@ from discord.ext import tasks
 from dotenv import load_dotenv
 
 import otomotoAPI
+from SearchTarget import SearchTarget
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 CHANNEL_ID = int(os.getenv('CHANNEL_ID'))
 client = discord.Client(intents=discord.Intents.all())
+
+searches = []
 
 @client.event
 async def on_ready():
@@ -47,7 +50,22 @@ async def on_message(message):
         user_id = message.author
         channel_id = message.channel.id
         search_url = message.content[len('$search'):].strip()
-        bots_message = await message.channel.send(f'ok {user_id.mention} i added search for your url (<{search_url}>)')
+        searches.append(SearchTarget(search_url, channel_id, user_id, datetime.now()))
+        await message.channel.send(f'ok {user_id.mention} i added search for your url (<{search_url}>)')
         await message.edit(suppress=True)   # remove users embedded content
+
+    if message.content.lower().startswith('$cancel'):
+        index = message.content[len('$cancel'):].strip()
+        if index == '':  # print all searches with indexes
+            all_searches = ''
+            for i, s in enumerate(searches):
+                all_searches = all_searches + f'\n({i}) added {s.add_date.strftime("%d.%m.%Y %H:%M:%S")} by {s.user_id.mention}: \n<{s.search_url}>'
+            embed = discord.Embed(title="to cancel search send `$cancel` command with corresponding index", description=all_searches,color=0xa30000)
+            await message.channel.send(embed=embed)
+        else:
+            index = int(index)
+            embed = discord.Embed(title="successfully canceled search", description=f'\n({index}) added {searches[index].add_date.strftime("%d.%m.%Y %H:%M:%S")} by {searches[index].user_id.mention}: \n<{searches[index].search_url}>', color=0xa30000)
+            del searches[index]
+            await message.channel.send(embed=embed)
 
 client.run(TOKEN)
